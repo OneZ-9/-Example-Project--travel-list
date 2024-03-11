6f5d36fd5dcf9f0bcde0b9a225df8168f669ebc4
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-03-11 22:34:37 +0530

    Add sort options to the PackedList

--- src/App.js
+++ src/App.js
@@ -94,10 +94,29 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItems, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  function handleSortOption(e) {
+    setSortBy(e.target.value);
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             itemProp={item}
             key={item.id}
@@ -106,6 +125,14 @@ function PackingList({ items, onDeleteItems, onToggleItems }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={handleSortOption}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by input description</option>
+          <option value="packed">Sort by input packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
